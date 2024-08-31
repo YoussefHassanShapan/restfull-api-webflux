965c027e4aa331373cf49a51dc0f4437f2510aac
youssefhassan <youssefhassanshapan1995@gmail.com>
2024-08-31 12:24:29 +0300

    Initial commit

---
+++ src/main/java/youssef/com/restfullwebflux/RetfullapiWebfluxApplication.java
@@ -0,0 +1,13 @@
+package youssef.com.retfullapiwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetfullapiWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetfullapiWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/youssef/com/restfullwebflux/RetfullapiWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package youssef.com.retfullapiwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetfullapiWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
